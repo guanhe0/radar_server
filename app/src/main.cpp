e36de01a9e7efa1e52a25ce4c7b0cc79ab7fead9
guanhe <307784732@qq.com>
2019-09-28 21:11:08 +0800

    add main.c c source code

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -17,5 +17,66 @@ void ServerObj::InitServerAddr()
 }
 int main()
 {
+	socklen_t ssize;
+	ssize = sizeof(struct sockaddr_in);
+	signal(SIGCHLD,sig_chld);
+	while(1){
+		if((connectfd = accept(listenfd,(struct sockaddr_in *)&client,&ssize)) == -1){
+			perror("accept() error\n");
+			exit(1);
+		}
+		if((pid = fork())>0){
+			close(connectfd);			
+			continue;
+		}
+		else if(pid == 0){
+			close(listenfd);
+			process_cli(connectfd,client);
+			exit(0);
+		}
+		else{
+			printf("fork error\n");
+			exit(0);
+		}
+	}
+	close(listenfd);
     return 0;    	    
+}
+
+void process_cli(int connectfd,sockaddr_in client)
+{
+	int num;
+	char  recvbuf[BUFSIZE],sendbuf[BUFSIZE],cli_name[BUFSIZE];
+	int len;
+	printf("You got a connection from %s",inet_ntoa(client.sin_addr));
+	
+	num = recv(connectfd,cli_name,BUFSIZE,0);
+	
+	if(num == 0){
+		close(connectfd);
+		printf("Client disconnected.\n");
+		return;
+	}
+	cli_name[num-1] = '\0';
+	printf("Client's name is %s. \n",cli_name);
+	while(num = recv(connectfd,recvbuf,BUFSIZE,0)){
+		recvbuf[num] = '\0';
+		printf("Received client (%s) message:%s",cli_name,recvbuf);
+		for(int i = 0; i < num - 1; i++){
+			sendbuf[i] = recvbuf[num - i - 2];
+		}
+		sendbuf[num-1] = '\0';
+		len = strlen(sendbuf);
+		send(connectfd,sendbuf,len,0);
+	}
+	close(connectfd);
+}
+
+void sig_chld(int signo)
+{
+	pid_t pid;
+	int stat;
+	while((pid = waitpid(-1,&stat,WNOHANG))>0);
+	printf("child %d terminated\n",pid);	
+	return;
 }
